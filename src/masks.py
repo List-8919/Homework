7fa0a7cdf1c5dfa4cb3ffce2c5b01c2027d335e2
KAA <list8919@gmail.com>
2026-04-05 19:19:51 +0300

    last homework

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функцию маскировки номера банковской карты"""
+    card_str = str(card_number)
+    mask = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
+
+    return mask
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функцию маскировки номера банковского счета"""
+    account_str = str(account_number)
+    return f"**{account_str[-4:]}"
+
+
+print(get_mask_account("73654108430135874305"))
+print(get_mask_card_number("73654108430135874305"))
