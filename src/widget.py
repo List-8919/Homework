ae018455afce0814a27777ea5cdb1a9e23bbff4c
KAA <list8919@gmail.com>
2026-04-05 22:14:25 +0300

    new homework version2

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,15 @@
-from masks import get_mask_card_number, get_mask_account
-
 # Импорт модуля re
 import re
 
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(account_card: str) -> str:
     """
-Принимает один аргумент — строку, содержащую тип и номер карты или счета,
-и возвращает строку с замаскированным номером.
+    Принимает один аргумент — строку, содержащую тип и номер карты или счета,
+    и возвращает строку с замаскированным номером.
     """
+
     if "Счет" in account_card:
         letters_count = "".join(re.findall(r"\D+", account_card))
         numbers_count = "".join(re.findall(r"\d+", account_card))
@@ -17,15 +19,19 @@ def mask_account_card(account_card: str) -> str:
         numbers_card = "".join(re.findall(r"\d+", account_card))
         return f"{letters_card} {get_mask_card_number(numbers_card)}"
 
+
 if __name__ == "__main__":
     print(mask_account_card("Visa Platinum 7000792289606361"))
-    print(mask_account_card("Maestro 7000792289606361"))
-    print(mask_account_card("Счет 73654108430135874305"))
+    print(mask_account_card("Visa Gold 5999414228426353"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(mask_account_card("Счет 35383033474447895560"))
 
-from datetime import datetime
 
-def get_date(date: str) -> str:
+def get_date(formated_date: str) -> str:
     """Функция преобразует дату в формат 'DD.MM.YYYY'"""
-    date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
-    return date.strftime("%d.%m.%Y")
+    date_split_list = formated_date.split('T')
+    formated_date = re.sub(r"(\d{4})-(\d{2})-(\d{2})", r"\3.\2.\1", date_split_list[0])
+    return formated_date
+
+
 print(get_date("2024-03-11T02:26:18.671407"))
