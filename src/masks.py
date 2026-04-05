19c8647e24cebda223c03589fc4895ecda718f55
KAA <list8919@gmail.com>
2026-04-05 20:00:41 +0300

    new homework

--- src/masks.py
+++ src/masks.py
@@ -10,7 +10,3 @@ def get_mask_account(account_number: str) -> str:
     """Функцию маскировки номера банковского счета"""
     account_str = str(account_number)
     return f"**{account_str[-4:]}"
-
-
-print(get_mask_account("73654108430135874305"))
-print(get_mask_card_number("Visa Platinum 7000792289606361"))
--- src/widget.py
+++ src/widget.py
@@ -17,12 +17,15 @@ def mask_account_card(account_card: str) -> str:
         numbers_card = "".join(re.findall(r"\d+", account_card))
         return f"{letters_card} {get_mask_card_number(numbers_card)}"
 
+if __name__ == "__main__":
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Maestro 7000792289606361"))
+    print(mask_account_card("Счет 73654108430135874305"))
+
 from datetime import datetime
 
 def get_date(date: str) -> str:
-    """
-    Функция преобразует дату в формат 'DD.MM.YYYY
-    '"""
+    """Функция преобразует дату в формат 'DD.MM.YYYY'"""
     date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
     return date.strftime("%d.%m.%Y")
 print(get_date("2024-03-11T02:26:18.671407"))
